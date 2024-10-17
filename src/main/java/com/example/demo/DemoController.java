a82be9d8a71388278d38d8695b62ac7c628c475d
Talha Tariq <talha.tariq@landregistry.gov.uk>
2024-10-18 00:37:53 +0100

    changes

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,12 +1,21 @@
 package com.example.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.example.Score;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RequestMapping("/")
 @RestController
+@CrossOrigin()
 public class DemoController {
 
     public DemoController() {
@@ -22,6 +31,14 @@ public class DemoController {
     @GetMapping("/hello")
     public String yes() {
         return "yesyesyesyesyyeysyeysgeuisgdsgiodugoidsfguoifdsugiosdfugoisdfuigo";
+    }
+
+    @PostMapping("/scores")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Score retreiveScores(@RequestBody Score score) {
+        System.out.println(score);
+
+        return new Score("yes checking this", "and this aswell", 55, 66);
     }
 
 }
---
+++ src/main/java/com/example/demo/DemoRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DemoRepository {
+
+    
+}
